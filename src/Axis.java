7ae90cc11b80db51387111223a58637f04ebb649
Ricardo Reiter <ricardof.reiter@gmail.com>
2016-06-28 15:02:23 -0300

    Ajustes para não fazer regra de três ao desenhar diagonais, agora só são aceitas diagonais em ângulo de 45º

--- src/Axis.java
+++ src/Axis.java
@@ -1,5 +1,4 @@
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.Touch;
 
 public class Axis {
 
@@ -11,40 +10,28 @@ public class Axis {
     private final RegulatedMotor motor;
     private int actualPos = 0;
 
-    public Axis(RegulatedMotor motor, int size, int rotationPerPos, int rotationDirection, Touch endCourseSensor) {
+    public Axis(RegulatedMotor motor, int size, int rotationPerPos, int rotationDirection) {
         this.motor = motor;
         this.rotationPerPos = rotationPerPos;
         this.size = size;
         this.rotationDirection = (byte) rotationDirection;
-
-//        motor.setSpeed(MAX_VELOCITY);
-//        if (rotationDirection < 0) {
-//            motor.forward();
-//        } else {
-//            motor.backward();
-//        }
-//
-//        while (!endCourseSensor.isPressed()) {}
-//        motor.stop();
     }
 
     public void waitCompleteMove() {
         motor.waitComplete();
     }
 
-    public void gotoPos(int pos, int velocity, boolean ignoreComplete) {
-        if (velocity > 0) {
-            if (pos > size || pos < 0) {
-                throw new ArrayIndexOutOfBoundsException();
-            }
-            motor.setSpeed(velocity);
-
-            int rotateQuantity = (pos - actualPos) * rotationPerPos;
-            rotateQuantity *= rotationDirection;
-
-            motor.rotate(rotateQuantity, ignoreComplete);
-            actualPos = pos;
+    public void gotoPos(int pos, boolean ignoreComplete) {
+        if (pos > size || pos < 0) {
+            throw new ArrayIndexOutOfBoundsException();
         }
+        motor.setSpeed(MAX_VELOCITY);
+
+        int rotateQuantity = (pos - actualPos) * rotationPerPos;
+        rotateQuantity *= rotationDirection;
+
+        motor.rotate(rotateQuantity, ignoreComplete);
+        actualPos = pos;
     }
 
 }
--- src/Printer.java
+++ src/Printer.java
@@ -1,7 +1,9 @@
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Float;
+import java.util.LinkedList;
+import java.util.List;
 
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.Touch;
 
 public class Printer {
 
@@ -17,43 +19,89 @@ public class Printer {
     private Pen pen;
     private Point2D currentPos;
 
-    public Printer(RegulatedMotor motorX, RegulatedMotor motorY, RegulatedMotor motorPen, Touch s1, Touch s2) {
-        xAxis = new Axis(motorX, X_SIZE, X_ROTATION_PER_POS, 1, s2);
-        yAxis = new Axis(motorY, Y_SIZE, Y_ROTATION_PER_POS, -1, s1);
+    public Printer(RegulatedMotor motorX, RegulatedMotor motorY, RegulatedMotor motorPen) {
+        xAxis = new Axis(motorX, X_SIZE, X_ROTATION_PER_POS, 1);
+        yAxis = new Axis(motorY, Y_SIZE, Y_ROTATION_PER_POS, -1);
         pen = new Pen(motorPen);
         currentPos = new Point2D.Float(0, 0);
     }
 
-    public void drawLine(Point2D from, Point2D to) {
-        if (!from.equals(currentPos)) {
-            gotoPos(from);
-            pen.setActive(false);
+    private static class Line {
+
+        private Point2D from;
+        private Point2D to;
+
+        public Line(Point2D from, Point2D to) {
+            super();
+            this.from = from;
+            this.to = to;
         }
-        pen.setActive(true);
-        gotoPos(to);
+
+        public Point2D getFrom() {
+            return from;
+        }
+
+        public Point2D getTo() {
+            return to;
+        }
+
+    }
+
+    public void drawMatrix(int[][] matrix) {
+        List<Line> lines = new LinkedList<Line>();
+
+        for (int y = 0; y < matrix.length; y++) {
+            Point2D lineFrom = null;
+            for (int x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 1) {
+                    if (lineFrom == null) {
+                        if (x == matrix[y].length - 1) {
+                            lines.add(new Line(new Float(x, y), new Float(x, y)));
+                        } else {
+                            lineFrom = new Float(x, y);
+                        }
+                    } else if (x == matrix[y].length - 1) {
+                        lines.add(new Line(lineFrom, new Float(x, y)));
+                    }
+                } else {
+                    if (lineFrom != null) {
+                        lines.add(new Line(lineFrom, new Float(x - 1, y)));
+                        lineFrom = null;
+                    }
+                }
+            }
+        }
+
+        for (Line line : lines) {
+            drawLine(line.getFrom(), line.getTo());
+        }
+        //        stop();
+    }
+
+    public void drawLine(Point2D from, Point2D to) {
+        System.out.println(String.format("From %s -> To %s", from, to));
+        //        if (!from.equals(currentPos)) {
+        //            gotoPos(from);
+        //            pen.setActive(false);
+        //        }
+        //        pen.setActive(true);
+        //        gotoPos(to);
     }
 
     public void stop() {
-        pen.setActive(false);
+        moveTo(new Float(0, 0), false);
     }
 
     private void gotoPos(Point2D pos) {
         int xDiff = (int) (pos.getX() - currentPos.getX());
         int yDiff = (int) (pos.getY() - currentPos.getY());
-        xDiff = xDiff * X_ROTATION_PER_POS;
-        yDiff = yDiff * Y_ROTATION_PER_POS;
 
-        int xSpeed = Axis.MAX_VELOCITY;
-        int ySpeed = Axis.MAX_VELOCITY;
-
-        if (Math.abs(xDiff) > Math.abs(yDiff)) {
-            ySpeed = (Math.abs(yDiff) * xSpeed) / Math.abs(xDiff);
-        } else {
-            xSpeed = (Math.abs(xDiff) * ySpeed) / Math.abs(yDiff);
+        if (xDiff != yDiff && xDiff != 0 && yDiff != 0) {
+            throw new RuntimeException("Desenho na vertical com ângulo diferente de 45º");
         }
 
-        xAxis.gotoPos((int) pos.getX(), xSpeed, true);
-        yAxis.gotoPos((int) pos.getY(), ySpeed, false);
+        xAxis.gotoPos((int) pos.getX(), true);
+        yAxis.gotoPos((int) pos.getY(), false);
         currentPos = pos;
         xAxis.waitCompleteMove();
         yAxis.waitCompleteMove();
